c9c18e5e9071355bc7b3d28180a3379c327f1b7f
xMarinex <39373096+xMarinex@users.noreply.github.com>
2018-11-27 11:48:18 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -42,6 +42,6 @@ if (timer.total < 1) {
 }
 
 window.onload = function (){
-    var deadline = new Date("may 25, 2018 00:00:01");
+    var deadline = new Date("may 25, 2019 00:00:01");
     startTimer("clock", deadline);
 }
